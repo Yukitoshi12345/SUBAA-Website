378c1e70511bdf343c140ff60a236b04377c880d
Yukitoshi <yukitoshi.imaizumizhou@gmail.com>
2024-06-04 02:05:03 +1000

    updated footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrl: './footer.component.css',
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+  currentYear: number = new Date().getFullYear(); // Initialize currentYear
 
+  constructor() {}
+
+  ngOnInit() {}
 }
